cafb0a7be04160ba5878a851f89182cf8cc7247b
dbp <byronpantoja@gmail.com>
2022-01-05 17:58:43 +0800

    Triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'John', age: 30 });
+const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newname', age: 31 });
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('save');
