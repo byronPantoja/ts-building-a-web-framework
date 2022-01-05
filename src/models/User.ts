093476017dcd1cb651999c72bd81d5aa408c5942
dbp <byronpantoja@gmail.com>
2022-01-05 17:31:09 +0800

    storing event listeners

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,10 @@ interface UserProps {
   age: number;
 }
 
+type Callback = () => {};
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -13,4 +16,6 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
+
+  on(eventName: string, callback: Callback) {}
 }
